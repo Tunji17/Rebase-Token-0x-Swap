e870d9df2e2a8bb91884c1892e0007e73e2403b3
tunji17 <tunjiabioye@outlook.com>
2023-03-21 12:48:01 +0200

    List and fill orders

--- client/src/components/BuyToken.tsx
+++ client/src/components/BuyToken.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import { useForm } from "react-hook-form";
-
+import { toast } from "react-hot-toast";
+import { useSigner } from "wagmi";
+import { Contract, utils, constants } from "ethers";
+import { getContractAddressesForChainOrThrow } from "@0x/contract-addresses";
+import { LimitOrder } from "@0x/protocol-utils";
 import { useToken } from '../context/tokenContext';
 import { useOrders } from '../context/orderContext';
-import proxyContract from '../utils/proxyContract.json';
+import exchangeProxyContract from '../utils/proxyContract.json';
 
 type FormData = {
   amount: number;
@@ -12,18 +16,84 @@ type FormData = {
 
 const BuyToken = () => {
   const { register, handleSubmit, watch, formState: { errors } } = useForm<FormData>();
+  const { allowances, contracts } = useToken();
   const { orders, fetchOrders, selectedOrder } = useOrders();
+  const watchAmount = watch("amount");
 
+  const { data: signer } = useSigner();
+
+  const order = React.useMemo(
+    () =>
+      orders.find((order) => order.metaData.orderHash === selectedOrder),
+    [orders, selectedOrder]
+  );
+
+  const sellingPrice = React.useMemo(
+    () =>
+    order
+        ? Number(utils.formatEther(order.order.makerAmount)) /
+          Number(utils.formatEther(order.order.takerAmount))
+        : 0,
+    [order]
+  );
+
+  const amount = React.useMemo(
+    () =>
+      watchAmount
+        ? watchAmount * sellingPrice
+        : constants.Zero,
+    [watchAmount, sellingPrice]
+  );
+
+  console.log("Amount", amount.toString());
+  
   const handleBuy = async (data: FormData) => {
+    const exchangeProxy = getContractAddressesForChainOrThrow(
+      137
+    ).exchangeProxy;
 
+    if (parseInt(allowances.mockToken) < watchAmount) {
+      try {
+        const txn = await contracts.mockToken?.connect(signer!).approve(exchangeProxy, constants.MaxUint256);
+        await txn.wait();
+      } catch (e) {
+        return;
+      }
+    }
+
+    const proxyContract = new Contract(
+      exchangeProxy,
+      exchangeProxyContract.abi,
+      signer!
+    );
+
+    try {
+      // create limit order
+      const limitOrder = new LimitOrder(order.order);
+      const txn = await proxyContract.fillLimitOrder(
+        limitOrder,
+        order.order.signature,
+        utils.parseEther(data.amount.toString())
+      );
+      await txn.wait();
+      toast.success("Order Filled");
+    } catch(e) {
+      toast.error("Order Failed");
+      return;
+    }
+
+    await fetchOrders();
   }
 
+  if (!selectedOrder) return null;
   return (
-
     <div className='m-10 p-10 w-full text-left border'>
       <h1 className='text-3xl'>Buy Token</h1>
-      <p className='text-gray-500'>Set Price: </p>
-      <form className='flex flex-col m-3' onSubmit={handleSubmit((data) => console.log(data))}>
+      <p className='text-gray-500'>Set Price: {sellingPrice}</p>
+      <p className='text-gray-500'>
+          You will recieve Amount: {amount.toString()} Rebase Token for {watchAmount || 0} Mock Token
+      </p>
+      <form className='flex flex-col m-3' onSubmit={handleSubmit((data) => handleBuy(data))}>
         <label>Amount of Mock Token</label>
         <input className='w-full border px-2 mb-2' type="number" {...register("amount", { required: true })} />
         <button className='bg-blue-700 hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow' type="submit">Submit</button>
--- client/src/components/ListOrders.tsx
+++ client/src/components/ListOrders.tsx
@@ -5,11 +5,11 @@ const ListOrders = () => {
   return (
     <div className='w-1/2'>
       <h1 className='text-3xl'>List Orders</h1>
-      <div className='flex flex-col p-4'>
+      <div className='flex flex-col p-4 space-y-4'>
         {orders.map(({ order, metaData }) => (
           <div
             onClick={() => selectOrder(metaData.orderHash)}
-           className={`text-left border p-4 w-fit cursor-pointer ${
+           className={`text-left border p-4 w-fit cursor-pointer space-y-2 ${
               selectedOrder === metaData.orderHash ? 'bg-gray-200' : ''
            }`} key={metaData.orderHash}>
             <p> Seller: { order.maker }</p>
